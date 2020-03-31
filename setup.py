bef7f7d366bb182aa15fe325c10341b8ea0135fb
Johannes Bauer <joe@johannes-bauer.com>
2020-03-31 23:36:40 +0200

    Release pdfminify on PyPi
    Since we've pushed llpdf to PyPi, we can now also migrate pdfminify the
    same way. Fixes #6.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name = "pdfminify",
+	packages = setuptools.find_packages(),
+	version = "0.2.0",
+	license = "gpl-3.0",
+	description = "PDF minification tool",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	author = "Johannes Bauer",
+	author_email = "joe@johannes-bauer.com",
+	url = "https://github.com/johndoe31415/pdfminify",
+	download_url = "https://github.com/johndoe31415/pdfminify/archive/0.2.0.tar.gz",
+	keywords = [ "pdf", "reader", "writer", "document", "parser" ],
+	install_requires = [ "llpdf>=0.0.4" ],
+	entry_points = {
+		"console_scripts": [
+			"pdfminify = pdfminify.__main__:main"
+		]
+	},
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+	],
+)
